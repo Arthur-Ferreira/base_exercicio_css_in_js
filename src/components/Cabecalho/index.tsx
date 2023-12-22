6b2bc4233ea049a5dc5462a7363681e441e610da
Arthur-Ferreira <arthurfdarosa@gmail.com>
2023-12-22 16:35:47 -0300

    Styled Components update, feature Header

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,20 @@
-import styles from './Cabecalho.module.css'
+import React from 'react'
+import styled from 'styled-components'
 
+// Componente Cabecalho que exibe o título "EBAC Jobs"
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <Header>
     <h1>EBAC Jobs</h1>
-  </header>
+  </Header>
 )
 
 export default Cabecalho
+
+/* Styled Components para o componente Cabecalho */
+
+const Header = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
